f2e101341fd45017df9ab7250a0b9b10a14be251
ioaneomerod <ioaneomerod@gmail.com>
2020-12-09 12:48:41 -1000

    Finished moving transect image scripts

---
+++ transectImagesAndGraphs/plotly24.js
@@ -0,0 +1,37 @@
+trace1 = {
+  meta: {
+    columnNames: {
+      labels: 'A',
+      values: 'B',
+      textposition: 'B'
+    }
+  },
+  mode: 'markers',
+  type: 'pie',
+  textinfo: 'percent',
+  labelssrc: 'kainyogi:57:3b4a35',
+  labels: ['seagrass', 'non-scleractinian coral', 'scleractinian coral', 'coralline algae', 'coral or coralline algae', 'macroalgae', 'turf algae', 'unclassified algae', 'emergent vegetation', 'Giant Clam', 'Other non-mobile inverts', 'None', 'unclassified'],
+  valuessrc: 'kainyogi:57:4e3a50',
+  values: ['0', '0', '0', '6', '0', '44', '109', '0', '0', '0', '0', '46', '0'],
+  textposition: 'inside',
+  texttemplate: '',
+  insidetextorientation: 'radial'
+};
+data = [trace1];
+layout = {
+  title: { text: 'Ratio of Living Cover Found' },
+  xaxis: {
+    range: [-1, 6],
+    autorange: true
+  },
+  yaxis: {
+    range: [-1, 4],
+    autorange: true
+  },
+  autosize: true,
+  piecolorway: ['#0d0887', '#46039f', '#7201a8', '#9c179e', '#bd3786', '#d8576b', '#ed7953', '#fb9f3a', '#fdca26', '#f0f921']
+};
+Plotly.plot('plotly24', {
+  data: data,
+  layout: layout
+});
---
+++ transectImagesAndGraphs/plotly29.js
@@ -0,0 +1,35 @@
+trace1 = {
+  meta: {columnNames: {
+      labels: 'A',
+      values: 'B',
+      textposition: 'B'
+    }},
+  mode: 'markers',
+  type: 'pie',
+  textinfo: 'percent',
+  labelssrc: 'kainyogi:95:4d0f77',
+  labels: ['unconsolidated', 'mud', 'sand', 'Hardbottom', 'rubble', 'boulder', 'rock', 'man-made'],
+  valuessrc: 'kainyogi:95:d1e616',
+  values: ['0', '0', '34', '0', '154', '0', '8', '0'],
+  textposition: 'inside',
+  texttemplate: '',
+  insidetextorientation: 'radial'
+};
+data = [trace1];
+layout = {
+  title: {text: 'Ratio of Substrate Found'},
+  xaxis: {
+    range: [-1, 6],
+    autorange: true
+  },
+  yaxis: {
+    range: [-1, 4],
+    autorange: true
+  },
+  autosize: true,
+  piecolorway: ['rgb(232, 245, 171)', 'rgb(220, 219, 137)', 'rgb(209, 193, 107)', 'rgb(199, 168, 83)', 'rgb(186, 143, 66)', 'rgb(170, 121, 60)', 'rgb(151, 103, 58)', 'rgb(129, 87, 56)', 'rgb(104, 72, 53)', 'rgb(80, 59, 46)', 'rgb(57, 45, 37)', 'rgb(34, 30, 27)']
+};
+Plotly.plot('plotly29', {
+  data: data,
+  layout: layout
+});
